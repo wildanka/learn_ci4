1be70f4e4c59b21ce4fab603390963e5d13c402f
Wildan Kurniadi <danprogramming10@gmail.com>
2021-03-20 23:34:12 +0700

    05 how to use views in ci4 (intro)

---
+++ app/Controllers/Pages.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controllers;
+
+class Pages extends BaseController
+{
+	public function index()
+	{
+		return view('home');
+	}
+
+	public function about()
+	{
+		return view('home');
+	}
+}
