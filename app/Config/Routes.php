0291404385c8a0669e112fc38223dc08e0889529
Wildan Kurniadi <danprogramming10@gmail.com>
2021-03-20 23:24:45 +0700

    #02 add get routing, modify controller, explore about routing controller/method/data finish exploring route placeholder

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -35,8 +35,8 @@ $routes->setAutoRoute(true);
 $routes->get('/', 'Home::index');
 $routes->get('/coba/index', 'Coba::index');
 $routes->get('/coba/about', 'Coba::about');
-// $routes->get('/coba/(:any)', 'Coba::about/$1');
-$routes->get('/coba/(:any)/(:num)', 'Coba::about/$1/$2');
+$routes->get('/coba/(:any)', 'Coba::about/$1');
+// $routes->get('/coba/(:any)/(:num)', 'Coba::about/$1/$2');
 $routes->get('/users', 'Admin\Users::index'); 
 
 /*
