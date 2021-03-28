95f2b074a0a8f45a91eec53c401b60f2803d1cf7
Wildan Kurniadi <danprogramming10@gmail.com>
2021-03-28 22:28:14 +0700

    30 use old request data so the inputted data that should be valid will still be shown if error occured

--- app/Views/comic/edit.php
+++ app/Views/comic/edit.php
@@ -11,7 +11,7 @@
                 <div class="form-group row">
                     <label for="inputTitle" class="col-sm-2 col-form-label">Judul</label>
                     <div class="col-sm-6">
-                        <input type="text" class="form-control <?= ($validation->hasError('title')) ? 'is-invalid' : ''; ?>" id="inputTitle" name="title" placeholder="cth: naruto, slamdunk, dll" value="<?= $comic['title']; ?>" autofocus>
+                        <input type="text" class="form-control <?= ($validation->hasError('title')) ? 'is-invalid' : ''; ?>" id="inputTitle" name="title" placeholder="cth: naruto, slamdunk, dll" value="<?= (old('title')) ? old('title') : $comic['title']; ?>" autofocus>
                         <div class="invalid-feedback">
                             <?= $validation->getError('title'); ?>
                         </div>
@@ -20,19 +20,19 @@
                 <div class="form-group row">
                     <label for="inputWritter" class="col-sm-2 col-form-label">Penulis</label>
                     <div class="col-sm-6">
-                        <input type="text" class="form-control" id="inputWritter" name="writter" placeholder="cth: mashasi kishimoto, aoyama gosho, takehiko inoue" value="<?= $comic['writter']; ?>">
+                        <input type="text" class="form-control" id="inputWritter" name="writter" placeholder="cth: mashasi kishimoto, aoyama gosho, takehiko inoue" value="<?= (old('writter')) ? old('writter') : $comic['writter']; ?>">
                     </div>
                 </div>
                 <div class="form-group row">
                     <label for="inputPublisher" class="col-sm-2 col-form-label">Publisher</label>
                     <div class="col-sm-6">
-                        <input type="text" class="form-control" id="inputPublisher" name="publisher" placeholder="cth: garammedia, alex media tiviindo" value="<?= $comic['publisher']; ?>">
+                        <input type="text" class="form-control" id="inputPublisher" name="publisher" placeholder="cth: garammedia, alex media tiviindo" value="<?= (old('publisher')) ? old('publisher') : $comic['publisher']; ?>">
                     </div>
                 </div>
                 <div class="form-group row">
                     <label for="inputCover" class="col-sm-2 col-form-label">Sampul</label>
                     <div class="col-sm-6">
-                        <input type="text" class="form-control" id="inputCover" name="cover" placeholder="url to cover image" value="<?= $comic['cover']; ?>">
+                        <input type="text" class="form-control" id="inputCover" name="cover" placeholder="url to cover image" value="<?= (old('cover')) ? old('cover') : $comic['cover']; ?>">
                     </div>
                 </div>
 
